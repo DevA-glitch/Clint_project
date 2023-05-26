a6b56c7febc720bc898d51b14e9175d77bb63c42
DevA-glitch <devarajan.r.1011@gmail.com>
2023-05-26 13:02:48 +0530

    filter

--- Scripts/Fliter.js
+++ Scripts/Fliter.js
@@ -1,14 +1,3 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-    document.getElementById("navbar-id-about").style.top = "0";
-  } else {
-    document.getElementById("navbar-id-about").style.top = "-50px";
-  }
-}
-
-
 document.addEventListener('DOMContentLoaded', function(event) {
   document.querySelector('body').style.opacity = 1
 })
@@ -42,3 +31,19 @@ filterButtons.forEach((button) => {
     }
   });
 });
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+    document.getElementById("navbar-id-about").style.top = "0";
+  } else {
+    document.getElementById("navbar-id-about").style.top = "-50px";
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  document.querySelector('body').style.opacity = 1
+})
